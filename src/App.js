5edd206b416456def52d12a239fc5f628264d8f4
DoctorTone <drtonesoftware@gmail.com>
2023-01-24 01:20:42 +0000

    Add tree model

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Float, Sky } from "@react-three/drei";
 
 import { Island } from "./components/Island.js";
+import { Tree } from "./components/Tree.js";
 
 const App = () => {
   return (
@@ -9,8 +10,9 @@ const App = () => {
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
       <Sky />
-      <Float>
-        <Island position={[0, 0, 0]} />
+      <Float speed={2}>
+        <Island position={[0, 0, 6]} />
+        <Tree position={[-1, 1.5, 6]} scale={0.05} />
       </Float>
       <Float>
         <Island position={[5, 0, 0]} />
