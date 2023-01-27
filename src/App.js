1e503a760d05a53a0c63d8e23ca5c20c3749d446
DoctorTone <drtonesoftware@gmail.com>
2023-01-27 00:01:02 +0000

    Add icon models and position

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Float, Sky } from "@react-three/drei";
+import { OrbitControls, Float, Sky, Text3D } from "@react-three/drei";
 
 import { Island } from "./components/Island.js";
 import { Tree } from "./components/Tree.js";
+import { Work } from "./components/Work.js";
 
 const App = () => {
   return (
@@ -13,12 +14,22 @@ const App = () => {
       <Float speed={2}>
         <Island position={[0, 0, 6]} />
         <Tree position={[-1, 1.5, 6]} scale={0.05} />
+        <Text3D
+          position={[-0.75, 1.5, 6]}
+          font="./fonts/helvetiker_regular.typeface.json">
+          DRT
+          <meshStandardMaterial color="orange" />
+        </Text3D>
       </Float>
       <Float>
         <Island position={[5, 0, 0]} />
+        <Work rotation-y={Math.PI / 2} scale={0.2} position={[5, 2.5, 0]} />
       </Float>
       <Float>
         <Island position={[-5, 0, 0]} />
+      </Float>
+      <Float>
+        <Island position={[0, 0, -5]} />
       </Float>
 
       <OrbitControls />
