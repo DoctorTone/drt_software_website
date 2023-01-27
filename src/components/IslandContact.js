e22f95f6372d9c6c5d4fdd01dd1a05f0409cf56f
DoctorTone <drtonesoftware@gmail.com>
2023-01-27 15:11:32 +0000

    New 3D text model

--- src/components/IslandContact.js
+++ src/components/IslandContact.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Float } from "@react-three/drei";
+import React, { Suspense } from "react";
+import { Float, Text } from "@react-three/drei";
 import { Island } from "./Island.js";
 import { Phone } from "./Phone.js";
 
@@ -7,7 +7,20 @@ export const IslandContact = () => {
   return (
     <Float>
       <Island position={[-5, 0, 0]} />
-      <Phone position={[-5, 2, 0]} scale={0.01} />
+      <Suspense fallback={null}>
+        <Text
+          color="white"
+          center
+          fontSize={0.2}
+          position={[-5, 2.5, 0]}
+          anchorX="center"
+          anchorY="middle"
+          outlineWidth={0.02}
+          outlineColor="black">
+          Contact
+        </Text>
+      </Suspense>
+      <Phone position={[-5, 2, 0]} scale={0.01}></Phone>
     </Float>
   );
 };
--- src/components/IslandDRT.js
+++ src/components/IslandDRT.js
@@ -1,19 +1,28 @@
-import React from "react";
-import { Float, Text3D } from "@react-three/drei";
+import React, { Suspense } from "react";
+import { Float, Text3D, Text } from "@react-three/drei";
 import { Island } from "./Island.js";
 import { Tree } from "./Tree.js";
+import { DRT } from "./DRT.js";
 
 export const IslandDRT = () => {
   return (
     <Float>
       <Island position={[0, 0, 6]} />
       <Tree position={[-1, 1.5, 6]} scale={0.05} />
-      <Text3D
-        position={[-0.75, 1.5, 6]}
-        font="./fonts/helvetiker_regular.typeface.json">
-        DRT
-        <meshStandardMaterial color="#E76F2A" />
-      </Text3D>
+      <DRT position={[0.2, 2.2, 6.4]} />
+      <Suspense fallback={null}>
+        <Text
+          color="white"
+          center
+          fontSize={0.2}
+          position={[0, 3, 6.4]}
+          anchorX="center"
+          anchorY="middle"
+          outlineWidth={0.02}
+          outlineColor="black">
+          About
+        </Text>
+      </Suspense>
     </Float>
   );
 };
