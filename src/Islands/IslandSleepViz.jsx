d1c080f5c342215544484aad4ff6a2162cd8feaa
DoctorTone <drtonesoftware@gmail.com>
2023-04-19 18:38:33 +0100

    Added sleep viz island

--- src/Islands/IslandSleepViz.jsx
+++ src/Islands/IslandSleepViz.jsx
@@ -5,7 +5,7 @@ import { Tablet } from "../Models/Tablet.jsx";
 import useSound from "use-sound";
 import useStore from "../state/store.js";
 
-const IslandSleepViz = ({ islandNumber }) => {
+export const IslandSleepViz = ({ islandNumber }) => {
   const [hovered, setHovered] = useState(false);
   const activeIsland = useStore((state) => state.activeIsland);
   const showSleepVizModal = useStore((state) => state.showSleepVizModal);
@@ -30,7 +30,32 @@ const IslandSleepViz = ({ islandNumber }) => {
   useCursor(hovered);
   const [play] = useSound("./sounds/select.wav", { volume: 0.25 });
 
-  return <div>IslandSleepViz</div>;
+  return (
+    <Float rotationIntensity={SCENE.rotationIntensity}>
+      <group
+        onPointerOver={pointerOver}
+        onPointerOut={pointerOut}
+        onClick={selectIsland}>
+        <Tablet
+          position={ISLANDS.MedVizModelPosition}
+          rotation={[Math.PI, 0, -Math.PI / 8]}
+          map={"/textures/sleepData.jpg"}
+        />
+        <Suspense fallback={null}>
+          <Text
+            color="white"
+            center
+            fontSize={SCENE.FONT_SIZE}
+            position={ISLANDS.MedVizTextPosition}
+            rotation-y={Math.PI / 2}
+            anchorX="center"
+            anchorY="middle"
+            outlineWidth={SCENE.FONT_OUTLINE_WIDTH}
+            outlineColor="black">
+            Sleep
+          </Text>
+        </Suspense>
+      </group>
+    </Float>
+  );
 };
-
-export default IslandSleepViz;
--- src/Scenes/DataVizScene.jsx
+++ src/Scenes/DataVizScene.jsx
@@ -3,6 +3,7 @@ import { IslandMedViz } from "../Islands/IslandMedViz";
 import { IslandBrainViz } from "../Islands/IslandBrainViz";
 import { IslandCovidViz } from "../Islands/IslandCovidViz";
 import { IslandFinanceViz } from "../Islands/IslandFinanceViz";
+import { IslandSleepViz } from "../Islands/IslandSleepViz";
 import { Island } from "../Islands/Island.jsx";
 import { Foundation } from "../Models/Foundation.jsx";
 
@@ -13,6 +14,7 @@ const DataVizScene = () => {
       <IslandFinanceViz islandNumber={0} />
       <IslandCovidViz islandNumber={1} />
       <IslandBrainViz islandNumber={2} />
+      <IslandSleepViz islandNumber={4} />
       <Foundation position={[1, 0, -1]} scale={2.5} />
       <Island position-y={-7.75} scale={5} />
     </>
