6758e999953592d2ecd4365795e328122bdb5ddb
DoctorTone <drtonesoftware@gmail.com>
2023-05-24 01:12:26 +0100

    Add second demo to shader island

--- src/Modals/ShaderModal.jsx
+++ src/Modals/ShaderModal.jsx
@@ -20,7 +20,7 @@ const ShaderModal = ({ showModal }) => {
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Shaders</Modal.Title>
+        <Modal.Title>Shaders/Materials</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
@@ -28,13 +28,25 @@ const ShaderModal = ({ showModal }) => {
         transformations, ranging from increased realism to weird and wonderful
         effects. The following demo shows a number of effects that you can
         switch between.
-        <div className="text-center mt-2">
+        <div className="text-center mt-2 mb-2">
           <Button
             href="https://drt-software.com/Demos/Effects/effects.html"
             target="_blank"
             className="me-2"
             variant="primary">
             Launch demo
+          </Button>
+        </div>
+        The material properties of objects can also create realistic effects.
+        The material editor below allows you to configure the physical material
+        on a 3D model.
+        <div className="text-center mt-2">
+          <Button
+            href="https://drt-software.com/Demos/Material/editor.html"
+            target="_blank"
+            className="me-2"
+            variant="warning">
+            Launch editor
           </Button>
         </div>
       </Modal.Body>
