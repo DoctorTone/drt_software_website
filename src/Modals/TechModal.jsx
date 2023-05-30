37172fa375f93b96377baeb35d33274c6a8aa81d
DoctorTone <drtonesoftware@gmail.com>
2023-05-30 22:45:36 +0100

    Update tech modal text

--- src/Modals/TechModal.jsx
+++ src/Modals/TechModal.jsx
@@ -24,12 +24,13 @@ const TechModal = ({ showModal }) => {
       </Modal.Header>
 
       <Modal.Body>
-        As the majority of my work is front-end web-based I use languages and
-        frameworks such as javascript, typescript, react, bootstrap, material
-        ui, etc. For all the graphics/WebXR programming I use three.js and react
-        three fiber and GLSL for shader programming. I have also built back-end
-        applications using node, express and database functionality. For
-        desktop/native graphical applications I also have experience of Unity.
+        As the majority of my work is 3D graphics and front-end web-based I use
+        languages and frameworks such as javascript, typescript, react,
+        bootstrap, material ui, etc. For all the graphics/WebXR programming I
+        use three.js and react three fiber and GLSL for shader programming. I
+        have also built back-end applications using node, express and database
+        functionality. For desktop/native graphical applications I also have
+        experience of Unity.
       </Modal.Body>
 
       <Modal.Footer>
