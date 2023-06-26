1cee15506fa91fc323e891a488d904f1dd7f00dc
DoctorTone <drtonesoftware@gmail.com>
2023-06-26 11:14:37 +0100

    Position speech bubble and change text

--- src/components/Bubble.jsx
+++ src/components/Bubble.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Bubble = () => {
   return (
     <div id="speechBubble" className="panel">
-      <p className="thought bubble">Bubble</p>
+      <p className="thought bubble">
+        Use the <strong>Rotate</strong> buttons to explore other islands
+      </p>
     </div>
   );
 };
