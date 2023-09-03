9b41a6b77335d6496e6d8df0269db6fc874a4c1c
DRT Software <tony@drt-software.com>
2023-09-03 18:37:58 +0100

    Removed all extra models

--- src/Islands/IslandDRT.jsx
+++ src/Islands/IslandDRT.jsx
@@ -7,68 +7,63 @@ import { SCENE, ISLANDS, MODALS } from "../state/Config.js";
 import useStore from "../state/store.js";
 
 export const IslandDRT = ({ slot }) => {
-	const [hovered, setHovered] = useState(false);
-	const [togglePoints, setTogglePoints] = useState(false);
-	const setVisibleModal = useStore((state) => state.setVisibleModal);
-	const [selectSound] = useState(() => new Audio("./sounds/select.wav"));
+  const [hovered, setHovered] = useState(false);
+  const [togglePoints, setTogglePoints] = useState(false);
+  const setVisibleModal = useStore((state) => state.setVisibleModal);
+  const [selectSound] = useState(() => new Audio("./sounds/select.wav"));
 
-	const selectIsland = () => {
-		if (slot === 0) {
-			setVisibleModal(MODALS.ABOUT);
-			selectSound.play();
-		}
-	};
+  const selectIsland = () => {
+    if (slot === 0) {
+      setVisibleModal(MODALS.ABOUT);
+      selectSound.play();
+    }
+  };
 
-	const pointerOver = () => {
-		if (slot === 0) {
-			setHovered(true);
-		}
-	};
+  const pointerOver = () => {
+    if (slot === 0) {
+      setHovered(true);
+    }
+  };
 
-	const pointerOut = () => {
-		setHovered(false);
-	};
+  const pointerOut = () => {
+    setHovered(false);
+  };
 
-	useCursor(hovered);
+  useCursor(hovered);
 
-	return (
-		<Float rotationIntensity={SCENE.rotationIntensity}>
-			<group
-				visible={slot >= 0}
-				onPointerOver={pointerOver}
-				onPointerOut={pointerOut}
-				onClick={selectIsland}
-				position={ISLANDS.SLOT_POSITIONS[slot]}
-			>
-				<TreePoints
-					showPoints={togglePoints}
-					position={ISLANDS.DRTTreePosition}
-					scale={0.05}
-				/>
-				<DRT position={ISLANDS.DRTModelPosition} />
-				<Shadow
-					scale={1.5}
-					opacity={0.65}
-					position={[
-						ISLANDS.DRTModelPosition[0],
-						ISLANDS.DRTModelPosition[1] - 0.5,
-						ISLANDS.DRTModelPosition[2],
-					]}
-				/>
-				<Island />
-				<Text
-					color="white"
-					center
-					fontSize={SCENE.FONT_SIZE}
-					position={ISLANDS.DRTTextPosition}
-					anchorX="center"
-					anchorY="middle"
-					outlineWidth={SCENE.FONT_OUTLINE_WIDTH}
-					outlineColor="black"
-				>
-					About
-				</Text>
-			</group>
-		</Float>
-	);
+  return (
+    <Float rotationIntensity={SCENE.rotationIntensity}>
+      <group
+        visible={slot >= 0}
+        onPointerOver={pointerOver}
+        onPointerOut={pointerOut}
+        onClick={selectIsland}
+        position={ISLANDS.SLOT_POSITIONS[slot]}
+      >
+        <DRT position={ISLANDS.DRTModelPosition} />
+        <Shadow
+          scale={1.5}
+          opacity={0.65}
+          position={[
+            ISLANDS.DRTModelPosition[0],
+            ISLANDS.DRTModelPosition[1] - 0.5,
+            ISLANDS.DRTModelPosition[2],
+          ]}
+        />
+        <Island />
+        <Text
+          color="white"
+          center
+          fontSize={SCENE.FONT_SIZE}
+          position={ISLANDS.DRTTextPosition}
+          anchorX="center"
+          anchorY="middle"
+          outlineWidth={SCENE.FONT_OUTLINE_WIDTH}
+          outlineColor="black"
+        >
+          About
+        </Text>
+      </group>
+    </Float>
+  );
 };
