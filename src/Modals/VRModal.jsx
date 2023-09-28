d25a15ce4ac311934df5a8e3a17ed9e40e79b147
DoctorTone <tony@drt-software.com>
2023-09-28 15:50:28 +0100

    Updated VR modal to reflect new VR framework

--- src/Modals/VRModal.jsx
+++ src/Modals/VRModal.jsx
@@ -6,47 +6,49 @@ import { MODALS } from "../state/Config.js";
 import useStore from "../state/store.js";
 
 const VRModal = ({ showModal }) => {
-  const setVisibleModal = useStore((state) => state.setVisibleModal);
-  const [show, setShow] = useState(false);
+	const setVisibleModal = useStore((state) => state.setVisibleModal);
+	const [show, setShow] = useState(false);
 
-  const handleClose = () => {
-    setShow(false);
-    setVisibleModal(MODALS.NONE);
-  };
+	const handleClose = () => {
+		setShow(false);
+		setVisibleModal(MODALS.NONE);
+	};
 
-  useEffect(() => {
-    setShow(showModal);
-  }, [showModal]);
+	useEffect(() => {
+		setShow(showModal);
+	}, [showModal]);
 
-  return (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Virtual Reality</Modal.Title>
-      </Modal.Header>
+	return (
+		<Modal show={show} onHide={handleClose}>
+			<Modal.Header closeButton>
+				<Modal.Title>Virtual Reality</Modal.Title>
+			</Modal.Header>
 
-      <Modal.Body>
-        With the advent of WebXR, it is now possible to have interactive,
-        virtual reality environments online. This opens up the possibilities to
-        develop simulations, games, educational apps and more, all in the
-        browser. Click below for a simple VR framework I use to experiment with.
-        <div className="text-center mt-2">
-          <Button
-            href="https://drt-software.com/Demos/VRFramework/VRFramework.html"
-            target="_blank"
-            className="me-2"
-            variant="primary">
-            Launch demo
-          </Button>
-        </div>
-      </Modal.Body>
+			<Modal.Body>
+				With the advent of WebXR, it is now possible to have interactive,
+				virtual reality environments online. This opens up the possibilities to
+				develop simulations, games, educational apps and more, all in the
+				browser. Click below to look around my experimental VR framework, and be
+				sure to check it regularly for updates!
+				<div className="text-center mt-2">
+					<Button
+						href="https://drt-software.com/Demos/VRFramework/VRFramework.html"
+						target="_blank"
+						className="me-2"
+						variant="primary"
+					>
+						Launch demo
+					</Button>
+				</div>
+			</Modal.Body>
 
-      <Modal.Footer>
-        <Button onClick={handleClose} variant="secondary">
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+			<Modal.Footer>
+				<Button onClick={handleClose} variant="secondary">
+					Close
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
 };
 
 export default VRModal;
