779f6398df1da763d833e3d2e62ac9fdd23d55a2
Tony Glover <tony@drt-software.com>
2024-04-24 11:52:41 +0100

    Recurring meteor animation

--- src/components/Meteor.jsx
+++ src/components/Meteor.jsx
@@ -7,16 +7,24 @@ const Meteor = () => {
     const [showMeteor, setShowMeteor] = useState(false);
     const texture = useTexture("./textures/meteor.png");
     const meteorRef = useRef();
-    
+
 useEffect(() => {
-    setTimeout(() => {
+    setInterval(() => {
         setShowMeteor(true);
+        // DEBUG
+        console.log("Meteor true")
     }, SCENE.METEOR_TIME * 1000);
 }, []);
 
     useFrame((state, delta) => {
         if(showMeteor) {
             meteorRef.current.position.x -= delta * SCENE.METEOR_SPEED;
+            if(meteorRef.current.position.x <= -SCENE.METEOR_START[0]) {
+                setShowMeteor(false);
+                meteorRef.current.position.x = SCENE.METEOR_START[0];
+                // DEBUG
+                console.log("Meteor false");
+            }
         }
     })
 
