e8ce7e85ba00e25caf3b06cc93524c7ebea07556
Tony Glover <tony@drt-software.com>
2024-05-08 11:05:46 +0100

    Updated why model colour

--- src/Models/Question.jsx
+++ src/Models/Question.jsx
@@ -6,12 +6,15 @@ Files: .\question.gltf [44.23KB] > C:\Users\tony\Documents\Github\drt_software_w
 
 import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { MeshStandardMaterial } from 'three'
 
 export function Question(props) {
   const { nodes } = useGLTF('./models/question.glb')
   return (
     <group rotation-y={-Math.PI/2} scale={2} {...props} dispose={null}>
-      <mesh geometry={nodes.Question.geometry} material={nodes.Question.material} rotation={[Math.PI / 2, 0, -Math.PI / 2]} />
+      <mesh geometry={nodes.Question.geometry} rotation={[Math.PI / 2, 0, -Math.PI / 2]}>
+        <meshStandardMaterial color={0xcc7306} />
+      </mesh>
     </group>
   )
 }
