b3d968468f2b45e3d86458bf151508d48a0410ef
DoctorTone <tony@drt-software.com>
2024-06-21 12:40:03 +0100

    Added full screen modal styling

--- src/Modals/AboutModal.jsx
+++ src/Modals/AboutModal.jsx
@@ -3,7 +3,9 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Image from "react-bootstrap/Image";
 import { MODALS } from "../state/Config.js";
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import useStore from "../state/store.js";
 
@@ -26,42 +28,58 @@ const AboutModal = ({ showModal }) => {
       contentClassName="mainModal"
       onHide={handleClose}
     >
-      <Container>
-        <Modal.Header closeButton>
-          <Modal.Title className="mx-auto text-center w-100">
-            Who am I?
-          </Modal.Title>
-        </Modal.Header>
+      <Modal.Header
+        className="border-0 pb-0 text-center mt-5"
+        closeButton
+        closeVariant="white"
+      >
+        <h2 className="mx-auto w-100 ps-3">Who am I ?</h2>
+      </Modal.Header>
 
-        <Modal.Body>
-          <p>
-            I'm Dr Tony Glover and I have more than 25 years experience in the
-            software development industry. I am passionate about all things 3D -
-            my mission is to create stunning interactive websites that showcase
-            the power of 3D graphics.
-          </p>
-          <p>
-            I've worked for both large and small commercial companies, start-ups
-            and academia. I've built applications in the fields of virtual
-            reality, data visualisation, product configuration, mental health,
-            children's games, graphical tools, architecture, medical imaging,
-            interactive experiences and lots more!
-          </p>
-          <p>
-            If you want to harness the magic of 3D in your application then
-            please get in touch!
-          </p>
-          <div className="text-center">
-            <Image src="./images/drt.jpg" fluid rounded />
-          </div>
-        </Modal.Body>
+      <Modal.Body>
+        <div className="text-center mb-3">
+          <Image src="./images/profile.jpg" fluid roundedCircle />
+        </div>
+        <Container>
+          <Row>
+            <Col xs={2}></Col>
+            <Col xs={8}>
+              <p>
+                I'm Dr Tony Glover and I have more than 25 years experience in
+                the software development industry. I am passionate about all
+                things 3D - my mission is to create stunning interactive
+                websites that showcase the power of 3D graphics.
+              </p>
+              <p>
+                I formed DRT in 2017 to provide 3D data visualisation services
+                to a number of companies. Since then I've had the pleasure of
+                working with some amazing people on all sorts of wonderful
+                projects.
+              </p>
+              <p>
+                I've worked with clients in both small and large companies, as
+                well as start-ups and academic institutions. I've worked on a
+                diverse array of projects including 3D editors for the
+                construction industry, data visualisation applications, virtual
+                museum exhibits, driving simulators, mental health trials and
+                even a brain surgery simulator!
+              </p>
+              <p>
+                I'm always open to new project ideas and collaborations and
+                would love to hear from you if you have an exciting and
+                challenging project that you need some help with. Feel free to
+                get in touch.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Body>
 
-        <Modal.Footer>
-          <Button onClick={handleClose} variant="secondary">
-            Close
-          </Button>
-        </Modal.Footer>
-      </Container>
+      <Modal.Footer className="border-0">
+        <Button onClick={handleClose} variant="secondary">
+          Close
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 };
