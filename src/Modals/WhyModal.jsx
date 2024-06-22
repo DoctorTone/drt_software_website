02147f636c59f71ffacbcf632bdefdcfb09ace11
DoctorTone <tony@drt-software.com>
2024-06-22 16:56:11 +0100

    Updated why modal

--- src/Modals/WhyModal.jsx
+++ src/Modals/WhyModal.jsx
@@ -11,6 +11,9 @@ import {
   Mouse2Fill,
   EyeFill,
   PencilFill,
+  PeopleFill,
+  BookHalf,
+  TrophyFill,
 } from "react-bootstrap-icons";
 
 import useStore from "../state/store.js";
@@ -50,24 +53,24 @@ const WhyModal = ({ showModal }) => {
           <Row>
             <Col xs={4}>
               <Mouse2Fill
-                className="mb-3 greyBackground p-2 rounded"
+                className="mb-3 greyBackground px-3 rounded"
                 color="orange"
                 size="60"
               />
               <p className="h5 mb-3">Enhanced engagement</p>
-              <p className="greyText">
+              <p className="greyText mb-5">
                 Users can explore and interact with realistic 3D models, making
                 the experience more memorable, and feel more involved.
               </p>
             </Col>
             <Col xs={4}>
               <EyeFill
-                className="mb-3 greyBackground p-2 rounded"
+                className="mb-3 greyBackground px-3 rounded"
                 color="orange"
                 size="60"
               />
               <p className="h5 mb-3">Improved visualisation</p>
-              <p className="greyText">
+              <p className="greyText mb-5">
                 Products can be represented realistically and viewed from any
                 angle. Complex data can be simplified and shared for a better
                 understanding.
@@ -75,18 +78,55 @@ const WhyModal = ({ showModal }) => {
             </Col>
             <Col xs={4}>
               <PencilFill
-                className="mb-3 greyBackground p-2 rounded"
+                className="mb-3 greyBackground px-3 rounded"
                 color="orange"
                 size="60"
               />
               <p className="h5 mb-3">Innovative Branding</p>
-              <p className="greyText">
+              <p className="greyText mb-5">
                 There are more innovative ways to represent your products. This
                 can greatly enhance the appearence compared to a traditional
                 website.
               </p>
             </Col>
+            <Col xs={4}>
+              <PeopleFill
+                className="mb-3 greyBackground px-3 rounded"
+                color="orange"
+                size="60"
+              />
+              <p className="h5 mb-3">Increased visitors</p>
+              <p className="greyText mb-5">
+                The interactive and engaging nature of the site will encourage
+                more users. Once there, they will be more tempted to stay
+              </p>
+            </Col>
+            <Col xs={4}>
+              <BookHalf
+                className="mb-3 greyBackground px-3 rounded"
+                color="orange"
+                size="60"
+              />
+              <p className="h5 mb-3">Compelling experiences</p>
+              <p className="greyText mb-5">
+                Visitors can explore interacive elements providing increased
+                depth and context.
+              </p>
+            </Col>
+            <Col xs={4}>
+              <TrophyFill
+                className="mb-3 greyBackground px-3 rounded"
+                color="orange"
+                size="60"
+              />
+              <p className="h5 mb-3">Competitive edge</p>
+              <p className="greyText mb-5">
+                3D interactive websites can provide a distinct advantage over a
+                more conventional website.
+              </p>
+            </Col>
           </Row>
+          <ThreeDots color="orange" size="30" />
         </Container>
       </Modal.Body>
 
