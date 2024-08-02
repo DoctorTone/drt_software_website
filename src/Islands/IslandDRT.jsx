2e773a697c3ca136e6a4e6d370dfc22721a4b695
DoctorTone <tony@drt-software.com>
2024-08-02 19:26:51 +0100

    Animate drt island text

--- src/Islands/IslandDRT.jsx
+++ src/Islands/IslandDRT.jsx
@@ -18,6 +18,7 @@ export const IslandDRT = ({ name, fadeIn, fadeOut, direction }) => {
   const setTransitionPhase = useStore((state) => state.setTransitionPhase);
   const setActiveIsland = useStore((state) => state.setActiveIsland);
 
+  const textMaterialRef = useRef();
   const textRef = useRef();
 
   const selectIsland = () => {
@@ -40,22 +41,23 @@ export const IslandDRT = ({ name, fadeIn, fadeOut, direction }) => {
 
   useCursor(hovered);
 
-  useFrame((state, delta) => {
+  useFrame((_, delta) => {
     if (fadeOutEnabled) {
-      textRef.current.opacity -= delta * SCENE.FADE_DELAY;
-      if (textRef.current.opacity < 0) {
-        textRef.current.opacity = 0;
+      textMaterialRef.current.opacity -= delta * SCENE.FADE_DELAY;
+      textRef.current.position.x += delta * direction;
+      if (textMaterialRef.current.opacity < 0) {
+        textMaterialRef.current.opacity = 0;
         fadeOutEnabled = false;
         setTransitionPhase(TRANSITIONS.FADE_IN);
       }
     }
     if (fadeInEnabled) {
-      if (textRef.current.opacity >= 1) {
-        textRef.current.opacity = 0;
+      if (textMaterialRef.current.opacity >= 1) {
+        textMaterialRef.current.opacity = 0;
       }
-      textRef.current.opacity += delta * SCENE.FADE_DELAY;
-      if (textRef.current.opacity >= 1) {
-        textRef.current.opacity = 1;
+      textMaterialRef.current.opacity += delta * SCENE.FADE_DELAY;
+      if (textMaterialRef.current.opacity >= 1) {
+        textMaterialRef.current.opacity = 1;
         fadeInEnabled = false;
         setTransitionPhase(TRANSITIONS.FADE_OUT);
         setActiveIsland(name);
@@ -89,6 +91,7 @@ export const IslandDRT = ({ name, fadeIn, fadeOut, direction }) => {
         />
         <IslandPoints />
         <Text
+          ref={textRef}
           color="white"
           center
           fontSize={SCENE.FONT_SIZE}
@@ -99,7 +102,7 @@ export const IslandDRT = ({ name, fadeIn, fadeOut, direction }) => {
           outlineColor="black"
         >
           About
-          <meshBasicMaterial ref={textRef} transparent={true} />
+          <meshBasicMaterial ref={textMaterialRef} transparent={true} />
         </Text>
       </group>
     </Float>
